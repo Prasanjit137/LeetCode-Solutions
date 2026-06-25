0cc5b1ba7820a59d2cee7933e327f9ef44a4eff2
Prasanjit Sarkar <38756751+Prasanjit137@users.noreply.github.com>
2026-06-25 23:21:37 +0530

    Add solution for Regular Expression Matching

---
+++ LeetCode/Medium/regular-expression-matching/solution.py
@@ -0,0 +1,25 @@
+// Regular Expression Matching
+// Difficulty: Medium
+// URL: https://leetcode.com/problems/regular-expression-matching/
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        memo = {}
+        def dfs(i, j):
+            if (i, j) in memo:
+                return memo[(i, j)]
+            if j == len(p):
+                return i == len(s)
+            
+            first_match = (i < len(s) and (p[j] == '.' or p[j] == s[i]))
+            
+            if j + 1 < len(p) and p[j+1] == '*':
+                # zero occurrences or one/more
+                res = (dfs(i, j+2) or (first_match and dfs(i+1, j)))
+            else:
+                res = first_match and dfs(i+1, j+1)
+            
+            memo[(i, j)] = res
+            return res
+        
+        return dfs(0, 0)
