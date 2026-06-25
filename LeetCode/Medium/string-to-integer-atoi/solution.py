a79e53d2734318cabcf0f367ed566d8245a21ce8
Prasanjit Sarkar <38756751+Prasanjit137@users.noreply.github.com>
2026-06-26 00:39:05 +0530

    Add solution for String to Integer (atoi)

---
+++ LeetCode/Medium/string-to-integer-atoi/solution.py
@@ -0,0 +1,34 @@
+// String to Integer (atoi)
+// Difficulty: Medium
+// URL: https://leetcode.com/problems/string-to-integer-atoi/
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        INT_MIN = -2**31
+        INT_MAX = 2**31 - 1
+        
+        i = 0
+        n = len(s)
+        while i < n and s[i] == ' ':
+            i += 1
+
+        if i == n:
+            return 0
+        sign = 1
+        if s[i] == '-':
+            sign = -1
+            i += 1
+        elif s[i] == '+':
+            i += 1
+        result = 0
+        while i < n and s[i].isdigit():
+            digit = ord(s[i]) - ord('0')
+            result = result * 10 + digit
+            i += 1
+
+        result *= sign
+        if result < INT_MIN:
+            return INT_MIN
+        if result > INT_MAX:
+            return INT_MAX
+        return result
